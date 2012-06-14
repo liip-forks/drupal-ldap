aefca4dc86d8b1b6a2032c49119127275cbbc599
johnbarclay <john@johnbarclay.com>
2012-06-14 09:37:11 -0500

    add of ldap servers api file

--- ldap_servers/ldap_servers.api.php
+++ ldap_servers/ldap_servers.api.php
@@ -38,3 +38,18 @@ function hook_ldap_attributes_needed_alter(&$attributes, $op, $server) {
   }
 
 }
+
+
+
+/**
+ * Allow the results from the ldap search answer to be modified
+ * The query parameters are provided as context infomation
+ * (readonly)
+ *
+ */
+function hook_ldap_server_search_results_alter($entries, $ldap_query_params) {
+  // look for a specific part of the $results array
+  // and maybe change it
+}
+
+
